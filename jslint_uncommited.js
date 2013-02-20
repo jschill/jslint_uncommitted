774e339c704d79bc98abbfb4389dfc78d52fdb52
Johannes Schill <johannes.schill@polyester.se>
2013-02-20 14:00:43 +0100

    Cleaned up

--- jslint_uncommited.js
+++ jslint_uncommited.js
@@ -1,16 +1,29 @@
 (function () {
-  var self = this,
-      exec = require('child_process').exec,
-      okRegExp = /No problems found/,
+  var exec = require('child_process').exec,
       jsExtRegExp = /\.js\s*$/,
       filesToLintCmd = 'git diff --name-only',
       gitRootCmd = 'git rev-parse --show-toplevel',
       lintSettings = '--indent 2 --maxerr 10 --node --devel --browser --sloppy --nomen --eqeq --vars --plusplus',
       lintPredef = '--predef Backbone';
 
-  exec(gitRootCmd, function (error, stdout, stderr) {
-    run(stdout.trim());
-  });
+  var lintFiles = function (gitRoot, files) {
+    var jsLintCmd = gitRoot + '/node_modules/.bin/jslint ' + lintSettings + ' ' + lintPredef,
+        isOkRegExp = / is OK\./, /* Example: "filname.js is OK." */
+        filename,
+        cmd;
+
+    files.forEach(function (file) {
+      filename = file.substr(file.lastIndexOf('/') + 1);
+      if (jsExtRegExp.test(file)) {
+        cmd = jsLintCmd + ' ' + gitRoot + '/' + file;
+        exec(cmd, function (error, stdout, stderr) {
+          if (((stdout.length > 0)  && !isOkRegExp.test(stdout)) || (stderr.length > 0)) {
+            throw "JSLint error in " + file + "\n" + stdout;
+          }
+        });
+      }
+    });
+  };
 
   var run = function (gitRoot) {
     exec(filesToLintCmd, function (error, stdout, stderr) {
@@ -18,18 +31,7 @@
     });
   };
 
-  var lintFiles = function (gitRoot, files) {
-    var jsLintCmd = gitRoot + '/node_modules/.bin/jslint ' + lintSettings + ' ' + lintPredef;
-    var cmd;
-    files.forEach(function (file) {
-      if (jsExtRegExp.test(file)) {
-        cmd = jsLintCmd + ' ' + gitRoot + '/' + file;
-        exec(cmd, function (error, stdout, stderr) {
-          if (((stdout.length > 0)  && !okRegExp.test(stdout)) || (stderr.length > 0)) {
-            throw "JSLint error in " + file + "\n" + stdout;
-          }
-        });
-      }
-    });
-  };
+  exec(gitRootCmd, function (error, stdout, stderr) {
+    run(stdout.trim());
+  });
 }());

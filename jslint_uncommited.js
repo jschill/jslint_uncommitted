08bce0d01d778592179f47153a74acd8e389b847
Johannes Schill <johannes.schill@polyester.se>
2013-02-21 11:28:56 +0100

    Bug fixes

--- jslint_uncommited.js
+++ jslint_uncommited.js
@@ -1,24 +1,31 @@
 (function () {
-  var exec = require('child_process').exec,
-      jsExtRegExp = /\.js\s*$/,
-      filesToLintCmd = 'git diff --name-only',
-      gitRootCmd = 'git rev-parse --show-toplevel',
-      lintSettings = '--indent 2 --maxerr 10 --node --devel --browser --sloppy --nomen --eqeq --vars --plusplus',
-      lintPredef = '--predef Backbone';
+  var fileToLint = process.argv[2],
+    exec = require('child_process').exec,
+    jsExtRegExp = /\.js\s*$/,
+    filesToLintCmd = 'git diff --name-only',
+    gitRootCmd = 'git rev-parse --show-toplevel',
+    lintSettings = '--indent 2 --maxerr 10 --node --devel --browser --sloppy --nomen --eqeq --vars --plusplus',
+    lintPredef = '--predef Backbone';
 
   var lintFiles = function (gitRoot, files) {
     var jsLintCmd = gitRoot + '/node_modules/.bin/jslint ' + lintSettings + ' ' + lintPredef,
-        isOkRegExp = / is OK\./, /* Example: "filname.js is OK." */
-        filename,
-        cmd;
+      isOkRegExp = / is OK\./, /* Example: "filname.js is OK." */
+      filename,
+      cmd;
+
+    // If single file, re-create as array
+    if (typeof files === 'string') {
+      files = [ files ];
+    }
 
     files.forEach(function (file) {
-      filename = file.substr(file.lastIndexOf('/') + 1);
       if (jsExtRegExp.test(file)) {
         cmd = jsLintCmd + ' ' + gitRoot + '/' + file;
         exec(cmd, function (error, stdout, stderr) {
           if (((stdout.length > 0)  && !isOkRegExp.test(stdout)) || (stderr.length > 0)) {
-            throw "JSLint error in " + file + "\n" + stdout;
+            console.log(stdout);
+          } else {
+            console.log('JSLint OK');
           }
         });
       }
@@ -26,9 +33,13 @@
   };
 
   var run = function (gitRoot) {
-    exec(filesToLintCmd, function (error, stdout, stderr) {
-      lintFiles(gitRoot, stdout.split("\n"));
-    });
+    if (fileToLint === undefined) {
+      exec(filesToLintCmd, function (error, stdout, stderr) {
+        lintFiles(gitRoot, stdout.split("\n"));
+      });
+    } else {
+      lintFiles(gitRoot, fileToLint);
+    }
   };
 
   exec(gitRootCmd, function (error, stdout, stderr) {
